33b5cc2221b528e1c3cf54b0e00fc6fea4f11c62
Alvaro <alvaro_benitez_m-tsapp1ma2122@cev.com>
2023-02-22 10:30:49 +0100

    carrousel in home page

--- Coppertaiment1/Controllers/HomeViewController.swift
+++ Coppertaiment1/Controllers/HomeViewController.swift
@@ -1,15 +1,87 @@
-
-import Foundation
 import UIKit
 
-class HomeViewController: UIViewController{
-    @IBAction func Profile(_ sender: UIButton) {
-        performSegue(withIdentifier: "profile", sender: sender)
-    }
+class HomeViewController: UIViewController {
+
     @IBAction func Basket(_ sender: Any) {
         performSegue(withIdentifier: "basket", sender: sender)
     }
+    @IBAction func Profile(_ sender: Any) {
+        performSegue(withIdentifier: "profile", sender: sender)
+    }
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageView: UIPageControl!
+    
+    var imgArr = [  UIImage(named:"1"),
+                    UIImage(named:"2") ,
+                    UIImage(named:"3") ,
+                    UIImage(named:"4") ]
+    
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pageView.numberOfPages = imgArr.count
+        pageView.currentPage = 0
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   @objc func changeImage() {
+    
+    if counter < imgArr.count {
+        let index = IndexPath.init(item: counter, section: 0)
+        self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+        pageView.currentPage = counter
+        counter += 1
+    } else {
+        counter = 0
+        let index = IndexPath.init(item: counter, section: 0)
+        self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+        pageView.currentPage = counter
+        counter = 1
+    }
+        
+    }
+
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let vc = cell.viewWithTag(111) as? UIImageView {
+            vc.image = imgArr[indexPath.row]
+        }
+        return cell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = sliderCollectionView.frame.size
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
     }
 }
