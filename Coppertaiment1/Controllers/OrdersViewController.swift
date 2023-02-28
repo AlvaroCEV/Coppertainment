18e020bbdeca7ba5d975dc66c7702d197fceca38
Alvaro <alvaro_benitez_m-tsapp1ma2122@cev.com>
2023-02-28 14:22:09 +0100

    profile menu image adapted

--- Coppertaiment1/Controllers/OrdersViewController.swift
+++ Coppertaiment1/Controllers/OrdersViewController.swift
@@ -5,15 +5,38 @@ import UIKit
 class OrdersViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
+        setPopUpButton()
     }
     @IBAction func Home(_ sender: Any) {
         performSegue(withIdentifier: "home", sender: sender)
     }
     @IBAction func Profile(_ sender: Any) {
-        performSegue(withIdentifier: "profile", sender: sender)
+        performSegue(withIdentifier: "CP", sender: sender)
     }
     @IBAction func Basket(_ sender: Any) {
         performSegue(withIdentifier: "basket", sender: sender)
     }
+    @IBOutlet weak var OrdersMenu: UIButton!
     
+    func setPopUpButton(){
+        let optionClosure = {(action: UIAction) in
+            if action.title == "Control panel" {
+                self.performSegue(withIdentifier: "CP", sender: nil)
+            }
+            if action.title == "Wishlist" {
+                self.performSegue(withIdentifier: "wishlist", sender: nil)
+            }
+            if action.title == "Abilities" {
+                self.performSegue(withIdentifier: "abilities", sender: nil)
+            }
+        }
+        OrdersMenu.menu = UIMenu(children : [
+            UIAction(title: "Control panel", handler: optionClosure),
+            UIAction(title: "Orders", state: .on, handler: optionClosure),
+            UIAction(title: "Wishlist", handler: optionClosure),
+            UIAction(title: "Abilities", handler: optionClosure)
+        ])
+        OrdersMenu.showsMenuAsPrimaryAction = true
+        OrdersMenu.changesSelectionAsPrimaryAction = true
+    }
 }
