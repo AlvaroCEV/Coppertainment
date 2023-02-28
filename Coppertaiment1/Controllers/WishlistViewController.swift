7f7ecd190851fb1f31a02789f36ccf309110e49d
Alvaro <alvaro_benitez_m-tsapp1ma2122@cev.com>
2023-02-28 12:02:02 +0100

    Profile full menu complete

--- Coppertaiment1/Controllers/WishlistViewController.swift
+++ Coppertaiment1/Controllers/WishlistViewController.swift
@@ -5,14 +5,38 @@ import UIKit
 class WishlistViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
+        setPopUpButton()
     }
     @IBAction func Home(_ sender: Any) {
         performSegue(withIdentifier: "home", sender: sender)
     }
     @IBAction func Profile(_ sender: Any) {
-        performSegue(withIdentifier: "profile", sender: sender)
+        performSegue(withIdentifier: "CP", sender: sender)
     }
     @IBAction func Basket(_ sender: Any) {
         performSegue(withIdentifier: "basket", sender: sender)
+    }
+    @IBOutlet weak var WishlistMenu: UIButton!
+    
+    func setPopUpButton(){
+        let optionClosure = {(action: UIAction) in
+            if action.title == "Control panel" {
+                self.performSegue(withIdentifier: "CP", sender: nil)
+            }
+            if action.title == "Orders" {
+                self.performSegue(withIdentifier: "orders", sender: nil)
+            }
+            if action.title == "Abilities" {
+                self.performSegue(withIdentifier: "abilities", sender: nil)
+            }
+        }
+        WishlistMenu.menu = UIMenu(children : [
+            UIAction(title: "Control panel", handler: optionClosure),
+            UIAction(title: "Orders", handler: optionClosure),
+            UIAction(title: "Wishlist", state: .on, handler: optionClosure),
+            UIAction(title: "Abilities", handler: optionClosure)
+        ])
+        WishlistMenu.showsMenuAsPrimaryAction = true
+        WishlistMenu.changesSelectionAsPrimaryAction = true
     }
 }
---
+++ Coppertaiment1/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Coppertaiment1
+//
+//  Created by Apps2M on 23/2/23.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var gameTitleLbl: UILabel!
+    
+    func setup(category: DishCategory) {
+        
+    }
+    
+}
