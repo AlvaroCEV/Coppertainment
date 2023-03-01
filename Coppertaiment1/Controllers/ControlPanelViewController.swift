974f38cd0cf7826db981afe4b3b5244f15edfc48
Alvaro <alvaro_benitez_m-tsapp1ma2122@cev.com>
2023-03-01 13:28:36 +0100

    profile almost finished and game activation design finished

--- Coppertaiment1/Controllers/ControlPanelViewController.swift
+++ Coppertaiment1/Controllers/ControlPanelViewController.swift
@@ -5,12 +5,36 @@ import UIKit
 class ControlPanelViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
+        setPopUpButton()
     }
     @IBAction func Home(_ sender: Any) {
         performSegue(withIdentifier: "home", sender: sender)
     }
     @IBAction func Basket(_ sender: Any) {
         performSegue(withIdentifier: "basket", sender: sender)
+    }
+    @IBOutlet weak var ProfileMenu: UIButton!
+    
+    func setPopUpButton(){
+        let optionClosure = {(action: UIAction) in
+            if action.title == "Orders" {
+                self.performSegue(withIdentifier: "orders", sender: nil)
+            }
+            if action.title == "Wishlist" {
+                self.performSegue(withIdentifier: "wishlist", sender: nil)
+            }
+            if action.title == "Abilities" {
+                self.performSegue(withIdentifier: "abilities", sender: nil)
+            }
+        }
+        ProfileMenu.menu = UIMenu(children : [
+            UIAction(title: "Abilities", handler: optionClosure),
+            UIAction(title: "Wishlist", handler: optionClosure),
+            UIAction(title: "Orders", handler: optionClosure),
+            UIAction(title: "Control panel", state: .on, handler: optionClosure)
+        ])
+        ProfileMenu.showsMenuAsPrimaryAction = true
+        ProfileMenu.changesSelectionAsPrimaryAction = true
     }
     
 }
